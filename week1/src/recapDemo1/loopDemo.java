590c40bb7992a7e1df577140d39b1c4421542a53
Samet Mete <samet-mete@hotmail.com>
2022-09-19 23:35:42 +0300

    loopDemo isimli uygulama eklendi.

---
+++ week1/src/recapDemo1/loopDemo.java
@@ -0,0 +1,15 @@
+package recapDemo1;
+
+import java.util.Iterator;
+
+public class loopDemo {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 10; i += 2) {
+			System.out.println(i);
+		}
+		System.out.println("Döngü bitti.");
+
+	}
+
+}
